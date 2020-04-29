e7883cde5598e9db665af8b8da928605aebd6715
Facundo.N <facundo.nadaf@gmail.com>
2020-04-29 16:35:57 -0300

    agregue un log 'hola mundo' al archivo js

--- puzzle/puzzle.js
+++ puzzle/puzzle.js
@@ -1,3 +1,4 @@
+console.log('hola mundo');
 let position = ['1', '2', '3', '4', '5', '6', '7', '8', '..'];
 let numbers = document.getElementById('btn-box');
 let resetButton = document.querySelector('.reset-game');
